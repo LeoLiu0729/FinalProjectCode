a6257cf78f97c311ff8db1b350dfa9f32239eeb8
LeoLiu0729 <142344403+LeoLiu0729@users.noreply.github.com>
2023-12-03 17:24:17 -0500

    Add files via upload

---
+++ security-jpa-demo/src/main/java/com/csc340/jpacruddemo/AppController.java
@@ -0,0 +1,44 @@
+package com.csc340.jpacruddemo;
+
+import com.csc340.jpacruddemo.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class AppController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping(value = {"", "/", "/dashboard", "/home"})
+    public String dashboard(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String role = auth.getAuthorities().toString();
+
+        if (role.contains("ADMIN")) {
+            return "index";
+        } else if (role.contains("BANKER")) {
+            return "banker_dashboard";
+        } else if (role.contains("USER")) {
+            return "customer_dashboard";
+        }
+
+        return "login";
+    }
+
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/403")
+    public String _403() {
+        return "403";
+    }
+}
